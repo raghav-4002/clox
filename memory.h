dffb11f12eef9a6661305b63724e6de74aaf2682
Raghav Sharma <code.practice17@gmail.com>
2025-11-14 14:12:17 +0530

    Added memory.h

---
+++ memory.h
@@ -0,0 +1,16 @@
+#ifndef MEMORY_H_ 
+#define MEMORY_H_
+
+
+#include "common.h"
+
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, old_count, new_count) \
+    (type*)reallocate(pointer, sizeof(type) * (old_count), \
+        sizeof(type) * (new_count))
+
+
+#endif //MEMORY_H_
